5541cb10a9633656a0dcde09242f8c1d4c95fec7
m-asikai <mikael.asikainen@metropolia.fi>
2026-04-09 13:57:58 +0300

    checkout sessioon data stripestä

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -3,38 +3,45 @@ import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_TEST_SECRET);
 
-const getPriceFromDashboard = async (priceId) => {
-  let price;
+const getPriceFromStripeDashboard = async (priceId) => {
+  let priceObject;
   if (!priceId) throw new Error("No price id provided.");
   try {
-    console.log();
-    price = await stripe.prices.retrieve(priceId);
+    // https://docs.stripe.com/api/prices/object
+    priceObject = await stripe.prices.retrieve(priceId, {
+      expand: ["product"],
+    });
+    return priceObject;
   } catch (e) {
     throw new Error("Failed to fetch price from Stripe.");
   }
-  return price;
 };
 
 export const createCheckoutSession = async (priceId, email) => {
-  const params = {
-    ...(email && { customer_email: email }),
-    line_items: [
-      {
-        price_data: {
-          currency: "eur",
-          unit_amount: (await getPriceFromDashboard(priceId)).unit_amount,
-          product_data: {
-            name: "backendistä",
-            description: "jahuu bingbäng",
+  try {
+    const priceData = await getPriceFromStripeDashboard(priceId);
+    const params = {
+      ...(email && { customer_email: email }),
+      line_items: [
+        {
+          price_data: {
+            currency: "eur",
+            unit_amount: priceData.unit_amount,
+            product_data: {
+              name: `${priceData.product.name} experience`,
+              description: priceData.product.description,
+            },
           },
+          quantity: 1,
         },
-        quantity: 1,
-      },
-    ],
-    mode: "payment",
-    success_url: `${process.env.FRONTEND_BASE_URL}success/`,
-    cancel_url: `${process.env.FRONTEND_BASE_URL}profile/`,
-  };
-  const session = await stripe.checkout.sessions.create(params);
-  return session.url;
+      ],
+      mode: "payment",
+      success_url: `${process.env.FRONTEND_BASE_URL}success/`,
+      cancel_url: `${process.env.FRONTEND_BASE_URL}profile/`,
+    };
+    const session = await stripe.checkout.sessions.create(params);
+    return session.url;
+  } catch (e) {
+    console.error(e);
+  }
 };
